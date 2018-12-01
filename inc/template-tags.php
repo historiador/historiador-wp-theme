63c34b7cbea6f968ed08b4fe8cb6cab8e9cdc842
Protesilaos Stavrou <info@protesilaos.com>
2018-12-01 10:57:39 +0200

    Remove byline. Escape html. No need to turn datetime into a permalink.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -15,15 +15,17 @@ if ( ! function_exists( 'historiador_posted_on' ) ) :
 	 */
 	function historiador_posted_on() {
 
-		// Get the author name; wrap it in a link.
-		$byline = sprintf(
-			/* translators: %s: post author */
-			__( 'by %s', 'historiador' ),
-			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . get_the_author() . '</a></span>'
-		);
-
-		// Finally, let's write all of this to the page.
-		echo '<span class="posted-on">' . historiador_time_link() . '</span><span class="byline"> ' . $byline . '</span>';
+// NOTE no need to display the byline.  Kept for reference.
+////		// Get the author name; wrap it in a link.
+////		$byline = sprintf(
+////			/* translators: %s: post author */
+////			__( 'by %s', 'historiador' ),
+////			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . get_the_author() . '</a></span>'
+////		);
+////
+////		// Finally, let's write all of this to the page.
+////		echo '<span class="posted-on">' . historiador_time_link() . '</span><span class="byline"> ' . $byline . '</span>';
+		echo '<span class="posted-on">' . historiador_time_link() . '</span>';
 	}
 endif;
 
@@ -37,20 +39,20 @@ if ( ! function_exists( 'historiador_time_link' ) ) :
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 		}
-
 		$time_string = sprintf(
 			$time_string,
-			get_the_date( DATE_W3C ),
-			get_the_date(),
-			get_the_modified_date( DATE_W3C ),
-			get_the_modified_date()
+			esc_attr( get_the_date( DATE_W3C ) ),
+			esc_html( get_the_date() ),
+			esc_attr( get_the_modified_date( DATE_W3C ) ),
+			esc_html( get_the_modified_date() )
 		);
+
 
 		// Wrap the time string in a link, and preface it with 'Posted on'.
 		return sprintf(
 			/* translators: %s: post date */
 			__( '<span class="screen-reader-text">Posted on</span> %s', 'historiador' ),
-			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+			$time_string
 		);
 	}
 endif;
