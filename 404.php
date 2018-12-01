7bb4140e3bb5505a19411383e799dfe2f39d23a6
Protesilaos Stavrou <info@protesilaos.com>
2018-12-01 12:14:50 +0200

    Add a proper error message. No need for silly interjections.

--- 404.php
+++ 404.php
@@ -16,9 +16,10 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+			<!-- TODO translate this -->
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'historiador' ); ?></h1>
+					<h1 class="page-title"><?php _e( 'ERROR 404: Page cannot be found.', 'historiador' ); ?></h1>
 				</header><!-- .page-header -->
 				<div class="page-content">
 					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'historiador' ); ?></p>
